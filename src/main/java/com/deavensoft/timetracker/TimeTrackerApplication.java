e25402b71440703aeff08dbf9b0404b48145d48e
Drazen Nikolic <drazen.nikolic@deavensoft.com>
2020-09-04 13:16:46 +0200

    Base Spring Boot project files.

---
+++ src/main/java/com/deavensoft/timetracker/TimeTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.deavensoft.timetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deavensoft/timetracker/TimeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deavensoft.timetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
